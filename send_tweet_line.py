63f75ae445e76497c836f2296e57f644192403c2
denpayanara <88641815+denpayanara@users.noreply.github.com>
2022-01-15 11:52:28 +0900

    Update and rename tweet.py to send_tweet_line.py

--- send_tweet_line.py
+++ send_tweet_line.py
@@ -16,7 +16,7 @@ musen = {
     '陸上移動中継局(包括免許)': [url_rep, img_rep]
     }
 
-def tweet(key, url):
+def send_message(key, url):
 
     df = pd.read_csv(url[0])
 
@@ -26,10 +26,14 @@ def tweet(key, url):
     # 差分ありの時
     if len(df_diff) > 0:
 
+        # Twitter
         api_key = os.environ["API_KEY"]
         api_secret = os.environ["API_SECRET_KEY"]
         access_token = os.environ["ACCESS_TOKEN"]
         access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+
+        # LINE
+        Line_Token = os.environ['LINE_TOKEN']
 
         # 都道府県の合計行を抽出
         df_pref = df_diff.query('市区町村名 == "滋賀県" or 市区町村名 == "京都府" or 市区町村名 == "大阪府" or 市区町村名 == "兵庫県" or 市区町村名 == "奈良県" or 市区町村名 == "奈良県" or 市区町村名 == "和歌山県"')
@@ -41,28 +45,38 @@ def tweet(key, url):
 
         text = "\n".join(pref)
 
-        tweet = f"{key}更新\n\n{text}\n\n奈良県の詳細\nhttps://script.google.com/macros/s/AKfycbx6OjIvfSwa9CJJ_arw5H08HwewIk7NXUGEDOX81f8Vi79piLqskVXpCO-o9Kw4ZrBQ3w/exec\n\n#楽天モバイル #近畿 #bot"
+        message = f"{key}更新\n\n{text}\n\n奈良県の詳細\nhttps://script.google.com/macros/s/AKfycbx6OjIvfSwa9CJJ_arw5H08HwewIk7NXUGEDOX81f8Vi79piLqskVXpCO-o9Kw4ZrBQ3w/exec\n\n#楽天モバイル #近畿 #bot"
 
-        print(tweet)
+        print(message)
 
-        auth = tweepy.OAuthHandler(api_key, api_secret)
-
-        auth.set_access_token(access_token, access_token_secret)
-
-        api = tweepy.API(auth)
-        
-        media_ids = []
+        # 画像読込
 
         img_url = url[1]
-
         img = requests.get(img_url).content
+        img_data = BytesIO(img)
 
-        res_media_ids = api.media_upload(filename='img.png', file=BytesIO(img))
+        # ツイート送信
 
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        media_ids = []
+        res_media_ids = api.media_upload(filename = 'img.png', file = img_data)
         media_ids.append(res_media_ids.media_id)
+        api.update_status(status = message, media_ids = media_ids)
 
-        api.update_status(status = tweet, media_ids = media_ids)
+        # LINE送信
+
+        token_dic = {'Authorization': 'Bearer' + ' ' + Line_Token}
+        send_dic = {'message': message} 
+        files = {'imageFile': img_data}
+        requests.post(
+            'https://notify-api.line.me/api/notify',
+            headers = token_dic,
+            data = send_dic,
+            files=files
+            )
 
 for key, url in musen.items():
 
-    tweet(key, url)
+    send_message(key, url)
